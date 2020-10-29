10e90dd0449d72d112a3c9bd6ed796a21a45f9ad
Ԝеѕ <5124946+wesinator@users.noreply.github.com>
2020-10-29 10:58:27 -0400

    https api

--- cybercure/__init__.py
+++ cybercure/__init__.py
@@ -4,12 +4,12 @@ useragent = 'cybercure-python-sdk/0.4.5'
 
 def get_url_indicators(typ='json',json=1):
 	""" Get URL indicators of compromise to use."""
-	response = requests.get("http://api.cybercure.ai/feed/get_url", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": typ })
+	response = requests.get("https://api.cybercure.ai/feed/get_url", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": typ })
 	return response.text
 
 def get_ip_indicators(type='json',return_type='json'):
 	""" Get IP indicators of compromise to use"""
-	response = requests.get("http://api.cybercure.ai/feed/get_ips", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": type })
+	response = requests.get("https://api.cybercure.ai/feed/get_ips", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": type })
 	if type == 'json' or type == 'stix' and return_type == 'json':
 		return response.json()
 	else:
@@ -17,10 +17,10 @@ def get_ip_indicators(type='json',return_type='json'):
 
 def get_hash_indicators(type='json',json=1):
 	""" Get Hash indicators of compromise"""
-	response = requests.get("http://api.cybercure.ai/feed/get_hash", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": type })
+	response = requests.get("https://api.cybercure.ai/feed/get_hash", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "type": type })
 	return response.text
 
 def search(indicator):
 	""" Perform search for indicators. """
-	response = requests.get("http://api.cybercure.ai/feed/search", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "value": indicator })
+	response = requests.get("https://api.cybercure.ai/feed/search", headers={ "Accept": "application/json",'user-agent': useragent }, params={ "value": indicator })
 	return response.json()
